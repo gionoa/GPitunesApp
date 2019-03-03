56ab702b69b1bd2301bb33bca5a53330cdc14e7b
gionoa <gnoa001@fiu.edu>
2019-03-03 17:25:53 -0500

    Add custom MediaTableViewCell

---
+++ GPitunesApp/Controllers/MediaTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MediaTableViewCell.swift
+//  GPitunesApp
+//
+//  Created by gnoa001 on 3/3/19.
+//  Copyright © 2019 Giovanni Noa. All rights reserved.
+//
+
+import UIKit
+
+class MediaTableViewCell: UITableViewCell {
+    static let reuseID = String(describing: MediaTableViewCell.self)
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
